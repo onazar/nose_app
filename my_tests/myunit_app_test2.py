8ee854c520c19442c03bd3a460f236813d7d1831
onazar <onazar@solanolabs.com>
2016-07-18 12:38:14 +0000

    added timeout to tests

--- my_tests/myunit_app_test2.py
+++ my_tests/myunit_app_test2.py
@@ -1,10 +1,12 @@
 import unittest
 import myunit.app
+import time
 
 class MyUnitTest(unittest.TestCase):
 
   def testInit(self):
     app = myunit.app.App(100)
+    time.sleep(20)
     self.assertEquals(app.var, 100)
 
   def testGetVar(self):
--- my_tests/myunit_app_test4.py
+++ my_tests/myunit_app_test4.py
@@ -1,9 +1,11 @@
 import unittest
 import myunit.app
+import time
 
 class MyUnitTest(unittest.TestCase):
 
   def testInit(self):
     app = myunit.app.App(100)
+    time.sleep(40)
     self.assertEquals(app.var, 100)
 
